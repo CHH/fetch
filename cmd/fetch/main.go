1f6c66f72f5394f36a278bfce78647c3399f7b39
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2014-08-04 17:39:19 +0200

    Use TCP

--- cmd/fetch/main.go
+++ cmd/fetch/main.go
@@ -6,17 +6,17 @@ import (
 	"net/rpc/jsonrpc"
 	"net"
 	"log"
-	"os"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	fetchService := fetch.NewService(fetch.Config{MaxIdleConnections: 10})
 	rpcServer := rpc.NewServer()
 	rpcServer.RegisterName("fetch", fetchService)
 
-	os.Remove("/tmp/fetch.sock")
-
-	l, err := net.Listen("unix", "/tmp/fetch.sock")
+	l, err := net.Listen("tcp", ":1337")
 	if err != nil {
 		log.Fatal(err)
 	}
